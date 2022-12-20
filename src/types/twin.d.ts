77752e0c3aafdf454bae85d5332b7705c77775c9
Bayathy <misosoup0321@icloud.com>
2022-12-20 15:16:05 +0900

    chore+ change eslint rules

--- src/types/twin.d.ts
+++ src/types/twin.d.ts
@@ -3,7 +3,7 @@ import {css as cssImport} from '@emotion/react'
 import styledImport from '@emotion/styled'
 // eslint-disable-next-line import/named
 import {CSSInterpolation} from '@emotion/serialize'
-
+import {TwComponentMap, TwFn} from "twin.macro";
 declare module 'twin.macro' {
     // The styled and css imports
     const styled: typeof styledImport
@@ -15,5 +15,6 @@ declare module 'react' {
     interface DOMAttributes<T> {
         tw?: string
         css?: CSSInterpolation
+
     }
 }

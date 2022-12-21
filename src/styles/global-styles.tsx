2f699257c9484e920c7efd1132e7625313fd82b4
Bayathy <misosoup0321@icloud.com>
2022-12-21 10:33:28 +0900

    chore: format code

--- src/styles/global-styles.tsx
+++ src/styles/global-styles.tsx
@@ -1,19 +1,19 @@
-import type {FC} from 'react'
-import { Global } from '@emotion/react'
-import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import type { FC } from "react";
+import { Global } from "@emotion/react";
+import tw, { css, theme, GlobalStyles as BaseStyles } from "twin.macro";
 
 const customStyles = css({
-    body: {
-        WebkitTapHighlightColor: theme`colors.purple.500`,
-        ...tw`antialiased`,
-    },
-})
+  body: {
+    WebkitTapHighlightColor: theme`colors.purple.500`,
+    ...tw`antialiased`,
+  },
+});
 
-const GlobalStyles : FC= () => (
-    <>
-        <BaseStyles />
-        <Global styles={customStyles} />
-    </>
-)
+const GlobalStyles: FC = () => (
+  <>
+    <BaseStyles />
+    <Global styles={customStyles} />
+  </>
+);
 
-export default GlobalStyles
+export default GlobalStyles;
